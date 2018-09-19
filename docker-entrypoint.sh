529df7330cc8ecd3cfcd9e590a4c01b9d77ed5dc
Javier Cortejoso <javier.cortejoso@gmail.com>
2018-09-19 12:41:27 +0200

    Wait for flag from keeper-contracts when contracts are ready

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,9 @@
 
 export CONFIG_FILE=/provider/oceandb.ini
 envsubst < /provider/oceandb.ini.template > /provider/oceandb.ini
-sleep 30
-
+echo "Waiting for contracts to be generated..."
+while [ ! -f "/usr/local/contracts/ready" ]; do
+  sleep 2
+done
 gunicorn -b ${PROVIDER_HOST}:${PROVIDER_PORT} -w ${PROVICER_WORKERS} provider.run:app
 tail -f /dev/null
