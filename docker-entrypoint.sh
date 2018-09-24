9c678b554ee5f0de5e94c50f7993d8abba8d3b94
Javier Cortejoso <javier.cortejoso@gmail.com>
2018-09-24 09:37:40 +0200

    New variable to enable waiting for contracts

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,15 +2,17 @@
 
 export CONFIG_FILE=/provider/oceandb.ini
 envsubst < /provider/oceandb.ini.template > /provider/oceandb.ini
-echo "Waiting for contracts to be generated..."
-while [ ! -f "/usr/local/keeper-contracts/ready" ]; do
-  sleep 2
-done
+if [ "${LOCAL_CONTRACTS}" = "true" ]; then
+  echo "Waiting for contracts to be generated..."
+  while [ ! -f "/usr/local/keeper-contracts/ready" ]; do
+    sleep 2
+  done
+fi
 market=$(python -c "import sys, json; print(json.load(open('/usr/local/keeper-contracts/OceanMarket.development.json', 'r'))['address'])")
 token=$(python -c "import sys, json; print(json.load(open('/usr/local/keeper-contracts/OceanToken.development.json', 'r'))['address'])")
 auth=$(python -c "import sys, json; print(json.load(open('/usr/local/keeper-contracts/OceanAuth.development.json', 'r'))['address'])")
 sed -i -e "/token.address =/c token.address = ${token}" /provider/oceandb.ini
-sed -i -e "/market.address =/c market.address = ${market}" /provider/oceandb.ini 
+sed -i -e "/market.address =/c market.address = ${market}" /provider/oceandb.ini
 sed -i -e "/auth.address =/c auth.address = ${auth}" /provider/oceandb.ini
 gunicorn -b ${PROVIDER_HOST}:${PROVIDER_PORT} -w ${PROVICER_WORKERS} provider.run:app
 tail -f /dev/null
