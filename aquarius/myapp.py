34c2329c666dbfe6ac578a079e0aafa348c5c67a
eruizgar91 <enrique@oceanprotocol.com>
2019-02-22 11:09:05 +0100

    Remove unnecessary block

--- aquarius/myapp.py
+++ aquarius/myapp.py
@@ -1,6 +1,5 @@
 import logging
 import os
-import sys
 
 from flask import Flask
 from flask_cors import CORS
@@ -11,9 +10,5 @@ CORS(app)
 if 'CONFIG_FILE' in os.environ and os.environ['CONFIG_FILE']:
     app.config['CONFIG_FILE'] = os.environ['CONFIG_FILE']
 else:
-    try:
-        app.config['CONFIG_FILE'] = 'config.ini'
-    except Exception as e:
-        logging.error('A config file must be set in the environment variable "CONFIG_FILE" or in config.ini')
-        logging.error(e)
-        sys.exit(1)
+    logging.info('Using default config: config.ini')
+    app.config['CONFIG_FILE'] = 'config.ini'
