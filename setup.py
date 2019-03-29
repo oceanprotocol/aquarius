aa50d61d4aece1248740417a561d45256693bc14
eruizgar91 <enrique@oceanprotocol.com>
2019-03-29 16:07:13 +0100

    Plecos version 0.7.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ install_requirements = [
     # 'oceandb-bigchaindb-driver==0.1.4',
     'PyYAML==4.2b4',
     'pytz==2018.5',
-    'plecos==0.6.0'
+    'plecos==0.7.0'
 ]
 
 setup_requirements = ['pytest-runner==2.11.1', ]
