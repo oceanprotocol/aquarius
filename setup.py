e8c3a6fa3daf5d93994c50c720639093f1316c5f
eruizgar91 <enrique@oceanprotocol.com>
2019-03-29 14:49:13 +0100

    Upgrade plecos version and release aquarius

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ install_requirements = [
     # 'oceandb-bigchaindb-driver==0.1.4',
     'PyYAML==4.2b4',
     'pytz==2018.5',
-    'plecos==0.5.2'
+    'plecos==0.6.0'
 ]
 
 setup_requirements = ['pytest-runner==2.11.1', ]
