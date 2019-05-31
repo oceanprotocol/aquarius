6771982b0f0f3d2e29eb873937599838b3d801b8
eruizgar91 <enrique@oceanprotocol.com>
2019-05-31 10:34:18 +0200

    Update plecos and validation order

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ install_requirements = [
     # 'oceandb-bigchaindb-driver==0.1.4',
     'PyYAML==5.1',
     'pytz==2018.5',
-    'plecos==0.7.7'
+    'plecos==0.7.8'
 ]
 
 setup_requirements = ['pytest-runner==2.11.1', ]
