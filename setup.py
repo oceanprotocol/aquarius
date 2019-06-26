e318cfffedd5a8b6ee0c14358d3d6c8a8754737f
eruizgar91 <enrique@oceanprotocol.com>
2019-06-26 14:27:07 +0200

    Bump version: 0.3.3 → 0.3.4

--- setup.py
+++ setup.py
@@ -78,6 +78,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/aquarius',
-    version='0.3.3',
+    version='0.3.4',
     zip_safe=False,
 )
