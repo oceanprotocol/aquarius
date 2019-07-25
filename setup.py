656ddca47e17b19a7ad46d25c93ba70b52034456
eruizgar91 <enrique@oceanprotocol.com>
2019-07-25 14:23:56 +0200

    add web3 dependency

--- setup.py
+++ setup.py
@@ -28,7 +28,8 @@ install_requirements = [
     # 'oceandb-bigchaindb-driver==0.1.4',
     'PyYAML==5.1',
     'pytz==2018.5',
-    'plecos==0.7.8'
+    'plecos==0.7.8',
+    'web3==4.5.0'
 ]
 
 setup_requirements = ['pytest-runner==2.11.1', ]
