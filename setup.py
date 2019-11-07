dc71b65fae26cf20c90c2aed934ff6ce2b5d1ab1
ssallam <samer@oceanprotocol.com>
2019-11-07 22:46:43 +0100

    Use latest plecos metadata validator.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ install_requirements = [
     # 'oceandb-bigchaindb-driver==0.1.4',
     'PyYAML==5.1',
     'pytz==2018.5',
-    'plecos==0.7.9'
+    'plecos==1.0.0'
 ]
 
 setup_requirements = ['pytest-runner==2.11.1', ]
