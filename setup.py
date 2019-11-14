65d09b8b035b09ee32183b8333581e0781fc8a2b
ssallam <samer@oceanprotocol.com>
2019-11-14 13:58:43 +0100

    Bump version: 1.0.1 → 1.0.2

--- setup.py
+++ setup.py
@@ -77,6 +77,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/aquarius',
-    version='1.0.1',
+    version='1.0.2',
     zip_safe=False,
 )
