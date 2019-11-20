5f02c3e9e5fe87b3ded9951b8aceb80858a866b2
ssallam <samer@oceanprotocol.com>
2019-11-20 15:20:20 +0100

    Fix date validation when updating ddo and update tests.

--- aquarius/app/assets.py
+++ aquarius/app/assets.py
@@ -432,12 +432,12 @@ def update(did):
         return msg, status
     _record = dict()
     _record = copy.deepcopy(data)
-    _record['created'] = datetime.strptime(data['created'], DATETIME_FORMAT)
+    _record['created'] = format_timestamp(data['created'])
     _record['service'] = _reorder_services(_record['service'])
     services = {s['type']: s for s in _record['service']}
     metadata_main = services['metadata']['attributes']['main']
-    metadata_main['dateCreated'] = datetime.strptime(metadata_main['dateCreated'], DATETIME_FORMAT)
-    metadata_main['datePublished'] = datetime.strptime(metadata_main['datePublished'], DATETIME_FORMAT)
+    metadata_main['dateCreated'] = format_timestamp(metadata_main['dateCreated'])
+    metadata_main['datePublished'] = format_timestamp(metadata_main['datePublished'])
     if not is_valid_dict_remote(_get_metadata(_record['service'])['attributes']):
         logger.error(_list_errors(list_errors_dict_remote,
                                   _get_metadata(_record['service'])['attributes']))
@@ -727,6 +727,10 @@ def _get_curation_metadata(services):
 def get_timestamp():
     """Return the current system timestamp."""
     return f'{datetime.utcnow().replace(microsecond=0).isoformat()}Z'
+
+
+def format_timestamp(timestamp):
+    return f'{datetime.strptime(timestamp, DATETIME_FORMAT).replace(microsecond=0).isoformat()}Z'
 
 
 def validate_date_format(date):
