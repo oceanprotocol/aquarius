fe0e891b77af56de6f39d2f6f4dee2452b47f16e
Alex <alex.coseru@gmail.com>
2020-03-22 14:11:00 +0200

    fix

--- aquarius/app/util.py
+++ aquarius/app/util.py
@@ -2,7 +2,7 @@
 #  SPDX-License-Identifier: Apache-2.0
 
 import logging
-
+import os
 from eth_account.messages import defunct_hash_message
 
 def get_signer_address(message, signature,web3,logger):
@@ -47,5 +47,28 @@ def _can_update_did(ddo, updated, signature,web3,logger):
     if address is None:
         return False
     if compare_eth_addresses(address, ddo['publicKey'][0]['owner'],web3) is True:
+        return True
+    return False
+
+def _can_update_did_from_allowed_updaters(ddo, updated, signature,web3,logger):
+    '''
+    Check if the signer is allowed to update the DDO. List of signers is taken from ENV variabile RATING_ALLOWED_UPDATER
+    :param record ddo: DDO that has to be updated
+    :param str updated: Updated field passed by user
+    :param str signature: Signature of the updated field, using web3.eth.personal.sign
+    :return: boolean TRUE if the signer is allowed to update the DDO
+    '''
+    allowedUpdater = os.environ.get("RATING_ALLOWED_UPDATER")
+    if ddo['updated'] is None or updated is None or ddo['updated']!=updated:
+        logger.debug("missmatch updated")
+        return False
+    address = get_signer_address(updated, signature,web3,logger)
+    if address is None:
+        logger.debug("signer_address is none")
+        return False
+    if allowedUpdater is None:
+        logger.debug("allowedUpdater is None")
+        return False
+    if compare_eth_addresses(address, allowedUpdater,web3) is True:
         return True
     return False
