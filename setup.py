861989e9a12fa63c2f8846d255c8506bc33b68ff
alexcos20 <alex.coseru@gmail.com>
2020-06-25 07:08:36 -0700

    Bump version: 1.1.0 → 1.2.0

--- setup.py
+++ setup.py
@@ -82,6 +82,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/aquarius',
-    version='1.1.0',
+    version='1.2.0',
     zip_safe=False,
 )
