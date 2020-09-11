bb5d8ff08bb69942289be0fe2d42a1e24acb9ec7
ssallam <travis@travis-ci.org>
2020-09-11 11:06:55 +0200

    Bump version: 2.0.0 → 2.0.1

--- setup.py
+++ setup.py
@@ -83,6 +83,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/aquarius',
-    version='2.0.0',
+    version='2.0.1',
     zip_safe=False,
 )
