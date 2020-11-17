1a5b13c4dde62fe0e4849b195391b24c1fc0c5ed
ssallam <travis@travis-ci.org>
2020-11-17 09:22:03 +0100

    add envvar to make running the server optional.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,5 +14,8 @@ if [ ${RUN_EVENTS_MONITOR} = "1" ]; then
     /aquarius/start_events_monitor.sh &
 fi
 
-gunicorn -b ${AQUARIUS_URL#*://} -w ${AQUARIUS_WORKERS} aquarius.run:app
+if [ ${RUN_AQUARIUS_SERVER} = "1" ]; then
+    gunicorn -b ${AQUARIUS_URL#*://} -w ${AQUARIUS_WORKERS} aquarius.run:app
+fi
+
 tail -f /dev/null
