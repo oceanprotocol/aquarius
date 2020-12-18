4a7207aa20490f452f5eff4ec9d08853f29ac754
Alex Coseru <alex.coseru@gmail.com>
2020-12-18 10:02:30 +0200

    filter multiple pools (#344)

--- aquarius/events/metadata_updater.py
+++ aquarius/events/metadata_updater.py
@@ -277,7 +277,7 @@ class MetadataUpdater:
             return None
 
         pools = [get_event_data(event_abi, log).address for log in logs]
-        return pools
+        return list(set(pools))
 
     def _get_liquidity_and_price(self, pools, dt_address):
         assert pools, f'pools should not be empty, got {pools}'
