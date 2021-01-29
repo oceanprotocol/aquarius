7dc1dcd5e819844e3ea175ea284938fbc11601cf
Calina Cenan <calina.tutunaru@algotech.solutions>
2021-01-29 16:10:06 +0200

    Adds endpoint to validate remoote DDO.

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,8 +3,8 @@
 import json
 import lzma
 import os
-import uuid
 
+from eth_account.messages import defunct_hash_message
 from web3 import Web3
 
 from aquarius.app.dao import Dao
@@ -14,9 +14,8 @@ from aquarius.run import get_status, get_version
 from tests.ddo_samples_invalid import json_dict_no_valid_metadata
 from tests.ddos.ddo_sample1 import json_dict
 from tests.ddos.ddo_sample_updates import json_before, json_valid
-from eth_account.messages import defunct_hash_message
-
-from tests.helpers import new_ddo, test_account1, send_create_update_tx, get_event, get_web3
+from tests.helpers import (get_event, get_web3, new_ddo, send_create_update_tx,
+                           test_account1)
 
 
 def sign_message(account, message_str):
@@ -69,16 +68,21 @@ def test_health(client):
 
 def test_post_with_no_valid_ddo(client, base_ddo_url, events_object):
     block = get_web3().eth.blockNumber
-    ddo = new_ddo(test_account1, get_web3(), f'dt.{block}', json_dict_no_valid_metadata)
+    ddo = new_ddo(
+        test_account1, get_web3(), f'dt.{block}', json_dict_no_valid_metadata
+    )
     ddo_string = json.dumps(dict(ddo.items()))
     _receipt = send_create_update_tx(
-        'create', ddo.id, bytes([1]), lzma.compress(Web3.toBytes(text=ddo_string)), test_account1)
+        'create', ddo.id, bytes([1]),
+        lzma.compress(Web3.toBytes(text=ddo_string)), test_account1
+    )
     get_event(EVENT_METADATA_CREATED, block, ddo.id)
     events_object.process_current_blocks()
     try:
         published_ddo = get_ddo(client, base_ddo_url, ddo.id)
-        assert not published_ddo, f'publish should fail, Aquarius validation ' \
-                                  f'should have failed and skipped the {EVENT_METADATA_CREATED} event.'
+        assert not published_ddo, 'publish should fail, Aquarius validation ' \
+                                  'should have failed and skipped the ' \
+                                  f'{EVENT_METADATA_CREATED} event.'
     except Exception:
         pass
 
@@ -141,12 +145,28 @@ def test_validate(client_with_no_data, base_ddo_url):
         base_ddo_url + '/validate', data={}
     )
     assert post.status_code == 200
-    assert post.data == b'[{"message":"\'main\' is a required property","path":""}]\n'
+    assert post.data == b'[{"message":"\'main\' is a required property","path":""}]\n'  # noqa
     post = run_request(
         client_with_no_data.post,
         base_ddo_url + '/validate',
         data=json_valid
     )
+    assert post.data == b'true\n'
+
+
+def test_validate_remote(client_with_no_data, base_ddo_url):
+    post = run_request(
+        client_with_no_data.post,
+        base_ddo_url + '/validate-remote', data={}
+    )
+    assert post.status_code == 400
+    assert post.data == b'{"message":"Invalid DDO format."}\n'
+    post = run_request(
+        client_with_no_data.post,
+        base_ddo_url + '/validate-remote',
+        data=json_before
+    )
+
     assert post.data == b'true\n'
 
 
