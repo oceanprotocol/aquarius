64d8a36c6cc23948e0604f4aa58ba4a66ae950f8
alexcos20 <alex.coseru@gmail.com>
2021-03-15 03:14:41 -0700

    fix bumps

--- setup.py
+++ setup.py
@@ -92,6 +92,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/oceanprotocol/aquarius",
-    version="version='2.2.5'",
+    version="2.2.5",
     zip_safe=False,
 )
