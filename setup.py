aebfb08999ea5908e64473732c68752b8291fd72
Alex Coseru <alex.coseru@gmail.com>
2021-03-25 13:06:37 +0200

    release 2.2.6

--- setup.py
+++ setup.py
@@ -92,6 +92,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/oceanprotocol/aquarius",
-    version="2.2.5",
+    version="2.2.6",
     zip_safe=False,
 )
