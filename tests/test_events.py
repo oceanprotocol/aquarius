b83ddf1a006700516f1b0b518164cf4e39b09c2b
Calina Cenan <calina@oceanprotocol.com>
2021-04-08 11:01:45 +0300

    Adds test for exchange created.

--- tests/test_events.py
+++ tests/test_events.py
@@ -11,6 +11,12 @@ from web3 import Web3
 import eth_keys
 
 from aquarius.events.constants import EVENT_METADATA_CREATED, EVENT_METADATA_UPDATED
+from ocean_lib.ocean.ocean_exchange import OceanExchange
+from aquarius.events.util import deploy_datatoken
+from ocean_lib.ocean.util import get_contracts_addresses
+from ocean_lib.config_provider import ConfigProvider
+from ocean_lib.models.fixed_rate_exchange import FixedRateExchange
+from ocean_lib.web3_internal.wallet import Wallet
 from tests.helpers import (
     get_web3,
     new_ddo,
@@ -21,6 +27,8 @@ from tests.helpers import (
     get_ddo,
     get_event,
 )
+
+_NETWORK = "ganache"
 
 
 def run_test(client, base_ddo_url, events_instance, flags=None, encryption_key=None):
@@ -103,3 +111,31 @@ def test_publish_unallowed_address(client, base_ddo_url, events_object):
     events_object.process_current_blocks()
     published_ddo = get_ddo(client, base_ddo_url, did)
     assert published_ddo is None
+
+
+def test_fixed_rate_events(client, base_ddo_url, events_object):
+    _ddo = new_ddo(test_account1, get_web3(), "dt.0")
+    did = _ddo.id
+    ddo_string = json.dumps(dict(_ddo))
+    data = Web3.toBytes(text=ddo_string)
+    send_create_update_tx("create", did, bytes([0]), data, test_account1)
+    events_object.process_current_blocks()
+    published_ddo = get_ddo(client, base_ddo_url, did)
+
+    dt_address_2 = deploy_datatoken(
+        get_web3(), test_account1.privateKey, "dt.1", "dt.1", test_account1.address
+    )
+    ox = OceanExchange(
+        dt_address_2, _get_exchange_address(), ConfigProvider.get_config()
+    )
+    test_account1_wallet = Wallet(get_web3(), private_key=test_account1.privateKey)
+    x_id = ox.create(_ddo["dataToken"], 0.9, test_account1_wallet)
+    events_object._process_pool_events()
+    published_ddo = get_ddo(client, base_ddo_url, did)
+    assert published_ddo["price"]["exchange_id"] == "0x" + x_id.hex()
+
+
+def _get_exchange_address():
+    return get_contracts_addresses(_NETWORK, ConfigProvider.get_config())[
+        FixedRateExchange.CONTRACT_NAME
+    ]
