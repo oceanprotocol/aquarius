729612a29f1e8f374708992f9fce3c7c9d1c35db
alexcos20 <alex.coseru@gmail.com>
2021-04-22 03:55:39 -0700

    Bump version: 2.2.10 → 2.2.11

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ setup(
     url="https://github.com/oceanprotocol/aquarius",
     # fmt: off
     # bumpversion needs single quotes
-    version='2.2.10',
+    version='2.2.11',
     # fmt: on
     zip_safe=False,
 )
