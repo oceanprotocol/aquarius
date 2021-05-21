a3d733da5050c1c0bd05b7fd8118e64df2bbfd1c
Calina Cenan <calina@oceanprotocol.com>
2021-05-21 11:37:14 +0300

    Adds plecos tests.

--- tests/conftest.cp.py
+++ tests/conftest.cp.py
@@ -2,80 +2,36 @@
 # Copyright 2021 Ocean Protocol Foundation
 # SPDX-License-Identifier: Apache-2.0
 #
-import os
 import json
+from pathlib import Path
 
 import pytest
-from pathlib import Path
-from ocean_lib.config import Config
-from ocean_lib.config_provider import ConfigProvider
-from ocean_lib.web3_internal.contract_handler import ContractHandler
-from ocean_lib.web3_internal.web3_provider import Web3Provider
 
-from aquarius.events.events_monitor import EventsMonitor
-from aquarius.constants import BaseURLs
-from aquarius.events.util import get_artifacts_path
-from aquarius.run import app
-from aquarius.plecos import plecos
+import plecos
 
-app = app
+# Select the latest schema path here
+# PATH_SCHEMA_DIR = Path().cwd() / 'plecos' / 'schemas'
+# print(PATH_SCHEMA_DIR)
+# PATH_LATEST_SCHEMA = PATH_SCHEMA_DIR / 'metadata_190218.json'
+# assert PATH_LATEST_SCHEMA.exists(), "Path not found: {}".format(PATH_LATEST_SCHEMA)
 
-EVENTS_INSTANCE = None
+# Local sample
 
-
-@pytest.fixture
-def base_ddo_url():
-    return BaseURLs.BASE_AQUARIUS_URL + "/assets/ddo"
-
-
-@pytest.fixture
-def client_with_no_data():
-    client = app.test_client()
-    client.delete(BaseURLs.BASE_AQUARIUS_URL + "/assets/ddo")
-    yield client
-
-
-@pytest.fixture
-def client():
-    client = app.test_client()
-
-    yield client
-
-
-@pytest.fixture
-def events_object():
-    global EVENTS_INSTANCE
-    if not EVENTS_INSTANCE:
-        config_file = os.getenv("CONFIG_FILE", "config.ini")
-        network_rpc = os.environ.get("EVENTS_RPC", "http://127.0.0.1:8545")
-
-        ConfigProvider.set_config(Config(config_file))
-        from ocean_lib.ocean.util import get_web3_connection_provider
-
-        Web3Provider.init_web3(provider=get_web3_connection_provider(network_rpc))
-        ContractHandler.set_artifacts_path(get_artifacts_path())
-
-        EVENTS_INSTANCE = EventsMonitor(
-            Web3Provider.get_web3(), app.config["CONFIG_FILE"]
-        )
-        EVENTS_INSTANCE.store_last_processed_block(0)
-    return EVENTS_INSTANCE
-
-
-PATH_SAMPLES_DIR = Path().cwd() / "tests" / "metadata_samples"
-
+PATH_SAMPLES_DIR = Path().cwd() / "plecos" / "samples"
 PATH_SAMPLE_METADATA_LOCAL = PATH_SAMPLES_DIR / "sample_metadata_local.json"
 assert PATH_SAMPLE_METADATA_LOCAL.exists(), "Path not found: {}".format(
     PATH_SAMPLE_METADATA_LOCAL
 )
 
+PATH_ALGORITHM_METADATA_LOCAL = PATH_SAMPLES_DIR / "algorithm_metadata_local.json"
+PATH_ALGORITHM_METADATA_REMOTE = PATH_SAMPLES_DIR / "algorithm_metadata_remote.json"
+
+# Remote sample
+PATH_SAMPLES_DIR = Path().cwd() / "plecos" / "samples"
 PATH_SAMPLE_METADATA_REMOTE = PATH_SAMPLES_DIR / "sample_metadata_remote.json"
 assert PATH_SAMPLE_METADATA_REMOTE.exists(), "Path not found: {}".format(
     PATH_SAMPLE_METADATA_REMOTE
 )
-
-PATH_ALGORITHM_METADATA_LOCAL = PATH_SAMPLES_DIR / "algorithm_metadata_local.json"
-PATH_ALGORITHM_METADATA_REMOTE = PATH_SAMPLES_DIR / "algorithm_metadata_remote.json"
 
 
 def _load_sample_path(path, msg):
@@ -84,7 +40,7 @@ def _load_sample_path(path, msg):
         with open(str(path)) as json_file:
             this_json = json.load(json_file)
     except TypeError as e:
-        print(f"error: {e}")
+        print(f"errpr: {e}")
     print(msg)
     return this_json
 
