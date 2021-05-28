de67ae5b96bac15929ac4c257487379bc9be209c
Alex Coseru <alex.coseru@gmail.com>
2021-05-28 10:33:23 +0300

    add encrypt_as_hex (#484)
    * add encrypt_as_hex
    
    * Bump version: 2.2.11 → 2.2.12

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ setup(
     url="https://github.com/oceanprotocol/aquarius",
     # fmt: off
     # bumpversion needs single quotes
-    version='2.2.11',
+    version='2.2.12',
     # fmt: on
     zip_safe=False,
 )
