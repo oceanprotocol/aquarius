a2d7b79fd4f361f27f2fa616c1a9e1d16e82f8b9
Calina Cenan <calina@cenan.net>
2021-06-17 09:24:38 +0000

    Remove case lowering.

--- aquarius/events/purgatory.py
+++ aquarius/events/purgatory.py
@@ -27,7 +27,7 @@ class Purgatory:
                 f"PURGATORY: Successfully retrieved new purgatory list from {env_var} env var."
             )
             return {
-                (a["did"].lower(), a["reason"])
+                (a["did"], a["reason"])
                 for a in response.json()
                 if a and "did" in a
             }
