6e2bfda798e71497acfaa5923cd82de6127df643
Călina Cenan <calina@kaolin.tech>
2021-07-12 09:19:03 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup_requirements = ["pytest-runner==5.3.1"]
 
 dev_requirements = [
     "bumpversion==0.6.0",
-    "pkginfo==1.7.0",
+    "pkginfo==1.7.1",
     "twine==3.4.1",
     "flake8",
     "isort",
