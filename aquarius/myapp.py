d161303ddb478ca440854bac44584fa4fb6b8bf1
Akshay <akshay.ap95@gmail.com>
2021-07-17 20:27:12 +0200

    Change base to stable-aqua

--- aquarius/myapp.py
+++ aquarius/myapp.py
@@ -2,10 +2,10 @@
 # Copyright 2021 Ocean Protocol Foundation
 # SPDX-License-Identifier: Apache-2.0
 #
-'''
+"""
 This module creates an instance of flask `app` and sets the environment configuration.
 If `CONFIG_FILE` is not found in environment variables, default `config.ini` file is used. 
-'''
+"""
 import logging
 import os
 
