2597c448ae77a2f67e72dbdaf53608194355702f
Akshay <akshay.ap95@gmail.com>
2021-07-19 19:15:04 +0200

    Feature: Improve docstrings

--- aquarius/block_utils.py
+++ aquarius/block_utils.py
@@ -14,6 +14,9 @@ class BlockProcessingClass(ABC):
         return ""
 
     def get_or_set_last_block(self):
+        """
+        :return: block number
+        """
         ignore_last_block = get_bool_env_value("IGNORE_LAST_BLOCK", 0)
         _block = int(os.getenv(self.block_envvar, 0))
         try:
