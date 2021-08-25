4ceb3f2230cdcfed39a108d732f31b7df380b2fd
Calina Cenan <calina@cenan.net>
2021-08-25 17:44:18 +0000

    Add full coverage to config.

--- tests/test_config.py
+++ tests/test_config.py
@@ -13,6 +13,18 @@ def test_config_from_text():
 
     config = Config(text=config_text)
     assert config.aquarius_url == "http://another-aqua.url"
+    assert config.allow_free_assets_only is False
+
+
+def test_config_from_env(monkeypatch):
+    config_text = """
+    [resources]
+    aquarius.url = http://another-aqua.url
+    """
+
+    monkeypatch.setenv("AQUARIUS_BIND_URL", "test")
+    config = Config(text=config_text)
+    assert config.aquarius_url == "test"
 
 
 def test_help():
