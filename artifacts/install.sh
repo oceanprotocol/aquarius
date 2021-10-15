2f2460d9fcd4d26d64b117a072abd5a62067c080
Calina Cenan <calina@cenan.net>
2021-10-15 07:54:05 +0000

    Add addresesses cp separately.

--- artifacts/install.sh
+++ artifacts/install.sh
@@ -1,2 +1,4 @@
 rm -rf $VIRTUAL_ENV/lib/python3.9/site-packages/artifacts/
+rm -rf $VIRTUAL_ENV/lib/python3.9/site-packages/artifacts/addresses
 cp -r ./artifacts $VIRTUAL_ENV/lib/python3.9/site-packages/artifacts/
+cp -r ./addresses $VIRTUAL_ENV/lib/python3.9/site-packages/artifacts/addresses
