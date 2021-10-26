142643d6472223a3bd832a18a09007186c852422
Calina Cenan <calina@cenan.net>
2021-10-26 09:42:31 +0000

    Fixes purgatory tests.

--- tests/test_purgatory.py
+++ tests/test_purgatory.py
@@ -2,7 +2,6 @@
 # Copyright 2021 Ocean Protocol Foundation
 # SPDX-License-Identifier: Apache-2.0
 #
-import json
 from web3 import Web3
 from datetime import datetime, timedelta
 from requests.models import Response
@@ -36,8 +35,7 @@ class PurgatoryForTesting(Purgatory):
 def publish_ddo(client, base_ddo_url, events_object):
     ddo = new_ddo(test_account1, get_web3(), "dt.0")
     did = ddo.id
-    data = Web3.toBytes(text=json.dumps(dict(ddo)))
-    send_create_update_tx("create", did, bytes([0]), data, test_account1)
+    send_create_update_tx("create", ddo, bytes([0]), test_account1)
     events_object.process_current_blocks()
 
     return did
