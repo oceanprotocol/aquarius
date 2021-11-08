66ff806ec7ddb544795550989e4622cc35e107c2
Calina Cenan <calina@oceanprotocol.com>
2021-11-08 10:26:21 +0200

    Install artifacts in GH Actions.

--- artifacts/install.sh
+++ artifacts/install.sh
@@ -1,5 +1,8 @@
-rm -rf $VIRTUAL_ENV/lib/python3.9/site-packages/artifacts/
-mkdir ${VIRTUAL_ENV}/lib/python3.9/site-packages/artifacts/
-cp -r ./addresses $VIRTUAL_ENV/lib/python3.9/site-packages/artifacts/addresses
+##
+## Copyright 2021 Ocean Protocol Foundation
+## SPDX-License-Identifier: Apache-2.0
+##
+rm -rf $VIRTUAL_ENV/lib/python3.8/site-packages/artifacts/
+mkdir ${VIRTUAL_ENV}/lib/python3.8/site-packages/artifacts/
 
-find ./artifacts -name '*.json' -exec cp -prv '{}' ${VIRTUAL_ENV}'/lib/python3.9/site-packages/artifacts/' ';'
+find ./artifacts -name '*.json' -exec cp -prv '{}' ${VIRTUAL_ENV}'/lib/python3.8/site-packages/artifacts/' ';'
