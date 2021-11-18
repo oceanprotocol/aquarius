f3427c0b91457686e695b69a75e37e025c722e3c
Pablo Maldonado <pablo@oceanprotocol.com>
2021-11-18 06:57:55 -0500

    Create MetadataStateProcessor and update state helper
    Signed-off-by: Pablo Maldonado <pablo@oceanprotocol.com>

--- aquarius/events/constants.py
+++ aquarius/events/constants.py
@@ -6,6 +6,8 @@
 Defines values of variables:
 1. `EVENT_METADATA_CREATED`
 2. `EVENT_METADATA_UPDATED`.
+3. `EVENT_METADATA_STATE`.
 """
 EVENT_METADATA_CREATED = "MetadataCreated"
 EVENT_METADATA_UPDATED = "MetadataUpdated"
+EVENT_METADATA_STATE = "MetadataState"
