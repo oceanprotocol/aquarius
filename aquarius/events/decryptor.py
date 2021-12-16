ec83bab7a992139d003439b5b745420b8b27f8e0
Călina Cenan <calina@kaolin.tech>
2021-12-16 20:55:54 +0200

    Updates nonce logic. (#647)

--- aquarius/events/decryptor.py
+++ aquarius/events/decryptor.py
@@ -2,6 +2,7 @@
 # Copyright 2021 Ocean Protocol Foundation
 # SPDX-License-Identifier: Apache-2.0
 #
+from datetime import datetime
 import os
 
 import requests
@@ -11,9 +12,11 @@ from eth_account.messages import encode_defunct
 
 def decrypt_ddo(w3, provider_url, contract_address, chain_id, txid):
     aquarius_account = Account.from_key(os.environ.get("PRIVATE_KEY"))
-    nonce, signature = get_nonce_and_signature(
-        w3, provider_url, aquarius_account, txid, chain_id
-    )
+    nonce = datetime.now().timestamp()
+    signature = aquarius_account.sign_message(
+        encode_defunct(text=f"{txid}{aquarius_account.address}{chain_id}{nonce}")
+    ).signature.hex()
+
     payload = {
         "transactionId": txid,
         "chainId": chain_id,
@@ -27,19 +30,3 @@ def decrypt_ddo(w3, provider_url, contract_address, chain_id, txid):
         return response.json()
 
     raise Exception(f"Provider exception on decrypt DDO: {response.content}")
-
-
-def get_nonce_and_signature(w3, provider_url, account, txid, chain_id):
-    response = requests.get(
-        provider_url + "/api/services/nonce", params={"userAddress": account.address}
-    )
-    if response.status_code == 200:
-        nonce = response.json()["nonce"]
-        return (
-            nonce,
-            account.sign_message(
-                encode_defunct(text=f"{txid}{account.address}{chain_id}{nonce}")
-            ).signature.hex(),
-        )
-
-    raise Exception(f"Provider exception on nonce retrieval: {response.content}")
