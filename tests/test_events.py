3cd7728d8c9132d87db95725fa77c2353185e875
David Hunt-Mateo <david@oceanprotocol.com>
2022-03-03 17:50:01 -0500

    Increase sleep time for test_events::test_metadata_state_update

--- tests/test_events.py
+++ tests/test_events.py
@@ -349,7 +349,7 @@ def test_metadata_state_update(client, base_ddo_url, events_object):
         ddo=_ddo, account=test_account1, state=MetadataStates.DEPRECATED
     )
     events_object.process_current_blocks()
-    time.sleep(5)
+    time.sleep(10)
     published_ddo = get_ddo(client, base_ddo_url, did)
     # Check if asset is soft deleted
     assert "id" not in published_ddo
@@ -368,7 +368,7 @@ def test_metadata_state_update(client, base_ddo_url, events_object):
         ddo=_ddo, account=test_account1, state=MetadataStates.ACTIVE
     )
     events_object.process_current_blocks()
-    time.sleep(5)
+    time.sleep(10)
     published_ddo = get_ddo(client, base_ddo_url, did)
     # Asset has been recreated
     assert published_ddo["id"] == did
