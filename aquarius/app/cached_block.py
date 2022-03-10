eab7bbc1c148110604ec6023ea829d3801c80b2f
Maria Carmina <mariacarmina.cretu@gmail.com>
2022-03-10 17:37:12 +0200

    More logs.

--- aquarius/app/cached_block.py
+++ aquarius/app/cached_block.py
@@ -25,6 +25,7 @@ def get_cached_block():
     :return: `cached_block` stored in the database
     """
     result = cache.get("cached_block")
+    logger.info(f"result:{result}")
     return result if result else None
 
 
@@ -33,7 +34,6 @@ def update_cached_block(cached_block):
     Updates the value of `cached_block` in the database
     :param: cached_block
     """
-    logger.info(f"cached_block:{cached_block}")
     if cached_block is None:
         logger.error("Cached block is None.")
         return
@@ -41,6 +41,7 @@ def update_cached_block(cached_block):
     block = get_or_create_cached_block(cached_block)
     logger.info(f"block:{block}")
     cache.set("cached_block", block)
+    logger.info(f'cache={cache.get("cached_block")}')
 
 
 def get_or_create_cached_block(cached_block):
