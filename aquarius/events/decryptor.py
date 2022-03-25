55739e73c9b1f075872bc0330e195884664989ff
Călina Cenan <calina@cenan.net>
2022-03-25 14:03:25 +0200

    Experimental 2 (#760)

--- aquarius/events/decryptor.py
+++ aquarius/events/decryptor.py
@@ -2,6 +2,7 @@
 # Copyright 2021 Ocean Protocol Foundation
 # SPDX-License-Identifier: Apache-2.0
 #
+import json
 import logging
 from datetime import datetime
 
@@ -40,11 +41,13 @@ def decrypt_ddo(w3, provider_url, contract_address, chain_id, txid, hash):
 
     if response.status_code == 201:
         if sha256(response.content).hexdigest() != hash.hex():
-            msg = f"Hash check failed: response={response.text}, encoded response={sha256(response.content).hexdigest()}\n metadata hash={hash.hex()}"
+            msg = f"Hash check failed: response={response.content}, encoded response={sha256(response.content).hexdigest()}\n metadata hash={hash.hex()}"
             logger.error(msg)
             raise Exception(msg)
         logger.info("Decrypted DDO successfully.")
-        return response.json()
+        response_content = response.content.decode("utf-8")
+
+        return json.loads(response_content)
 
     msg = f"Provider exception on decrypt DDO: {response.content}\n provider URL={provider_url}, payload={payload}."
     logger.error(msg)
--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ setup(
     url="https://github.com/oceanprotocol/aquarius",
     # fmt: off
     # bumpversion needs single quotes
-    version='4.0.1',
+    version='4.0.2',
     # fmt: on
     zip_safe=False,
 )
