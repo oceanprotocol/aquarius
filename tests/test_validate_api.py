339fb5004b763e6b3118a32ddef36b65e25442a5
Călina Cenan <calina@cenan.net>
2022-03-31 15:42:09 +0300

    Adds RBAC validation. (#770)

--- tests/test_validate_api.py
+++ tests/test_validate_api.py
@@ -3,7 +3,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 import json
-from unittest.mock import patch
+from requests.models import Response
+from unittest.mock import patch, Mock
 
 from tests.ddos.ddo_sample1_v4 import json_dict
 from tests.helpers import run_request_octet, run_request
@@ -104,3 +105,20 @@ def test_non_octet_non_dict(client, base_ddo_url, monkeypatch):
         data["error"]
         == "Invalid payload. The request could not be converted into a dict."
     )
+
+
+def test_validate_through_rbac(client, base_ddo_url, monkeypatch):
+    monkeypatch.setenv("RBAC_SERVER_URL", "test")
+
+    with patch("requests.post") as mock:
+        response = Mock(spec=Response)
+        response.json.return_value = False
+        mock.return_value = response
+
+        rv = run_request_octet(
+            client.post, base_ddo_url + "/validate", data=json.dumps({"test": "test"})
+        )
+
+        data = rv.get_json()
+        assert rv.status_code == 400
+        assert data["error"] == "DDO marked invalid by the RBAC server."
