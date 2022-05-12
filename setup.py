fbf0f028748b42ffb79259960704ac7de8b4e7bc
David Hunt-Mateo <david@oceanprotocol.com>
2022-05-12 14:19:21 -0400

    Bump flask from 2.0.2 to 2.1.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("CHANGELOG.md") as history_file:
 
 install_requirements = [
     "coloredlogs==15.0.1",
-    "Flask==2.0.2",
+    "Flask==2.1.2",
     "Flask-Cors==3.0.10",
     "flask-swagger==0.2.14",
     "flask-swagger-ui==3.36.0",
