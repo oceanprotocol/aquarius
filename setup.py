5dbeb8413ddfba97fc22bce6b95ba0ec64adaed8
Alex Coseru <alex.coseru@gmail.com>
2022-05-17 11:56:54 +0300

    bump contracts to alpha 33 (#803)
    * bump contracts to alpha 33
    
    * Bump version: 4.0.7 → 4.0.8

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ install_requirements = [
     "elasticsearch==7.17.0",
     "PyYAML==6.0",
     "pytz==2022.1",
-    "ocean-contracts==1.0.0a32",
+    "ocean-contracts==1.0.0a33",
     "web3==5.29.0",
     "gevent",
     "json-sempai==0.4.0",
@@ -95,7 +95,7 @@ setup(
     url="https://github.com/oceanprotocol/aquarius",
     # fmt: off
     # bumpversion needs single quotes
-    version='4.0.7',
+    version='4.0.8',
     # fmt: on
     zip_safe=False,
 )
