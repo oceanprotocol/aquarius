0a3f79ff354ca271c8ed37bc3cd47f95498052bf
alexcos20 <alex.coseru@gmail.com>
2022-05-17 15:38:57 -0700

    Bump version: 4.0.8 → 4.0.9

--- aquarius/__init__.py
+++ aquarius/__init__.py
@@ -9,5 +9,5 @@
 __author__ = """OceanProtocol"""
 # fmt: off
 # bumpversion needs single quotes
-__version__ = '4.0.8'
+__version__ = '4.0.9'
 # fmt: on
--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ setup(
     url="https://github.com/oceanprotocol/aquarius",
     # fmt: off
     # bumpversion needs single quotes
-    version='4.0.8',
+    version='4.0.9',
     # fmt: on
     zip_safe=False,
 )
