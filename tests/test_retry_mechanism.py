66fe3250edd7eb2e5b44eb56d6f7551497babd8e
Calina Cenan <calina@cenan.net>
2022-05-17 15:56:31 +0000

    Possible fix for freezer.

--- tests/test_retry_mechanism.py
+++ tests/test_retry_mechanism.py
@@ -2,8 +2,8 @@
 # Copyright 2021 Ocean Protocol Foundation
 # SPDX-License-Identifier: Apache-2.0
 #
-from datetime import datetime, timedelta
 from freezegun import freeze_time
+from datetime import datetime, timedelta
 from hashlib import sha256
 import json
 import os
