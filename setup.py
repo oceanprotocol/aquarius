60b13c189196efa776d080a85f95c91c3f23c0e3
Calina Cenan <calina@cenan.net>
2022-05-18 05:25:29 +0000

    Remove old env vars from workflow.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ install_requirements = [
     "elasticsearch==7.17.0",
     "PyYAML==6.0",
     "pytz==2022.1",
-    "ocean-contracts==1.0.0a33",
+    "ocean-contracts==1.0.0a34",
     "web3==5.29.0",
     "gevent",
     "json-sempai==0.4.0",
