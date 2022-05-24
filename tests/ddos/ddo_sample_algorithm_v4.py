6eff421cf2c011f614d6218d019a3dc6881a3086
Călina Cenan <calina@cenan.net>
2022-05-24 08:01:21 +0300

    Update ddo_sample_algorithm_v4.py

--- tests/ddos/ddo_sample_algorithm_v4.py
+++ tests/ddos/ddo_sample_algorithm_v4.py
@@ -36,7 +36,7 @@ algorithm_ddo_sample = {
                 "tag": "10",
                 "checksum": "test",
             },
-            "consumerParameters": {"name": "test_key"},
+            "consumerParameters": [{"name": "test_key"}, {"name": "another_test_key"}],
         },
         "additionalInformation": {
             "description": "Workflow to aggregate weather information",
@@ -61,7 +61,7 @@ algorithm_ddo_sample = {
                 "publisherTrustedAlgorithmPublishers": [],
             },
             "files": "encryptedFiles",
-            "consumerParameters": {"name": "test_key"},
+            "consumerParameters": [{"name": "test_key"}, {"name": "naming_this_key"],
         }
     ],
 }
