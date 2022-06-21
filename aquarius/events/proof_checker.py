0d18604f8ad9bed0785c04851d207105fa88e409
Calina Cenan <calina@cenan.net>
2022-06-21 15:47:50 +0000

    Adds unit tests.

--- aquarius/events/proof_checker.py
+++ aquarius/events/proof_checker.py
@@ -1,3 +1,5 @@
+import json
+from json import JSONDecodeError
 import logging
 import os
 
@@ -5,21 +7,32 @@ logger = logging.getLogger(__name__)
 
 
 def check_metadata_proofs(web3, metadata_proofs):
+    allowed_validators = os.getenv("ALLOWED_VALIDATORS")
+
+    if not allowed_validators:
+        return True
+
     try:
-        allowed_validators = os.getenv("ALLOWED_VALIDATORS")
+        allowed_validators = set(json.loads(allowed_validators))
+    except (JSONDecodeError, TypeError, Exception) as e:
+        logger.error(f"Reading list of allowed validators failed: {e}.")
 
-        if not allowed_validators:
-            return True
+        return False
 
-        proof_addresses = [
-            web3.toChecksumAddress(metadata_proofs.args.validator)
+    if not allowed_validators:
+        return True
+
+    try:
+        proof_addresses = {
+            web3.toChecksumAddress(metadata_proof.args.validator)
             for metadata_proof in metadata_proofs
-        ]
-        allowed_addresses = [
-            web3.toChecksumAddress(address) for address in allowed_validators
-        ]
+        }
 
-        allowed_addresses = list(set(proof_addresses) & set(allowed_addresses))
+        allowed_addresses = {
+            web3.toChecksumAddress(address) for address in allowed_validators
+        }
+
+        allowed_addresses = list(proof_addresses & allowed_addresses)
 
         return bool(allowed_addresses)
     except Exception:
